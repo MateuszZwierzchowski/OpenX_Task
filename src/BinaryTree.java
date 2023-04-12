35372a88d2157ed8b5ad51f9ad0fab7de78afd24
Mateusz Zwierzchowski <zwierzchowski.email@gmail.com>
2023-04-12 16:25:26 +0200

    update

---
+++ src/BinaryTree.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+public class BinaryTree {
+    Vector<Node> nodes;
+
+    public BinaryTree(){
+
+    }
+
+    public void load(){
+
+    }
+
+}
---
+++ src/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    int value;
+    Node leftNode;
+    Node rightNode;
+
+    public Node(int valueToBeSet){
+        value=valueToBeSet;
+        leftNode = null;
+        rightNode = null;
+    }
+
+
+}
